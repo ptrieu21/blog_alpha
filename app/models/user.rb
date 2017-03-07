45843bc1351b8efc7bcffbdf5661ace14561fdac
ptrieu21 <vuongtrieulk@gmail.com>
2017-03-07 13:42:24 +0000

    Add user data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false},
-            format: { with VALID_EMAIL_REGEX }
+            format: { with: VALID_EMAIL_REGEX }
 end
